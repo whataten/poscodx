fda142bf3b162ecd15d6281bbb178d583c2a8b5e
whataten <tiktaalik135462@gmail.com>
2024-05-01 15:37:01 +0900

    TCPClient #2

---
+++ network/src/main/java/test/TCPClient.java
@@ -0,0 +1,59 @@
+package test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class TCPClient {
+
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 5000;
+
+    public static void main(String[] args) {
+        Socket socket = null;
+        
+        try {
+            // 1. socket 생성
+            socket = new Socket();
+
+            // 2. 서버 연결
+            socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+
+            // 3. IO Stream 받아오기
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+
+            // 4. 쓰기
+            String data = "Hello World";
+            os.write(data.getBytes("utf-8"));
+
+            // 5. 읽기
+            byte[] buffer = new byte[256];
+            int readByteCount = is.read(buffer);
+            if(readByteCount == -1) {
+                System.out.println("[client] closed by server");
+                return;
+            }
+
+            data = new String(buffer, 0, readByteCount, "utf-8");
+            System.out.println("[client] received: " + data);
+
+        } catch (SocketException e) {
+            System.out.println("[client] error: " + e);
+        } catch (IOException e ) {
+            System.out.println("[client] suddenly closed by server");
+        }finally {
+            try {
+                if(socket != null && !socket.isClosed()){
+                    socket.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,8 +1,11 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.SocketException;
 
 public class TCPServer {
     public static void main(String[] args) {
@@ -12,21 +15,64 @@ public class TCPServer {
             serverSocket = new ServerSocket();
 
             // 2. 바인딩(binding)
-            //    Socket에 InetSocketAddress(InetAddress(IPAddress) + Port)를 바인딩한다.
+            // Socket에 InetSocketAddress(InetAddress(IPAddress) + Port)를 바인딩한다.
             // IPAddress: 0.0.0.0: 특정 호스트 IP를 바인딩 하지 않는다.
 
             // 백로그 : 작업을 놓치지 않기 위해 큐에 넣어놓는데 이 큐의 크기
             serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000), 10);
 
             // 3. accept
-            var socket = serverSocket.accept(); //blocking
+            var socket = serverSocket.accept(); // blocking
 
-            System.out.println("연결!!!!");
+            try {
+                var inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+                String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+                int remotePort = inetRemoteSocketAddress.getPort();
+                System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
+
+                // 4. IO Stream 받아오기
+                InputStream is = socket.getInputStream(); // byte 단위
+                OutputStream os = socket.getOutputStream();
+
+                while(true) {
+                    // 5. 데이터 읽기
+                    byte[] buffer = new byte[256];
+                    int readByteCount = is.read(buffer); // blocking
+
+                    if (readByteCount == -1) {
+                        // 클라이언트가 정상적으로 종료(close() 호출)
+                        System.out.println("[server] closed by client");
+                        break;
+                    }
+
+                    String data = new String(buffer, 0, readByteCount, "utf-8");
+                    System.out.println("[server] received: " + data);
+
+                    // 6. 데이터 쓰기
+                    os.write(data.getBytes("utf-8"));
+
+
+                }
+            } catch (SocketException e) {
+                System.out.println("[server] suddenly closed by client");
+            } catch (IOException e) {
+                System.out.println("[server] error: " + e);
+            }
+                finally {
+                try {
+                    if (socket != null && !socket.isClosed()) {
+                        socket.close();
+                    }
+                } catch (Exception e) {
+                    System.out.println("[server] error: " + e);
+                }
+            }
+
         } catch (IOException e) {
             System.out.println("[server] error: " + e);
         } finally {
             try {
-                if(serverSocket != null && !serverSocket.isClosed()) {
+                if (serverSocket != null && !serverSocket.isClosed()) {
                     serverSocket.close();
                 }
             } catch (IOException e) {
