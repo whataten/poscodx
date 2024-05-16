850bc8c2a71abbc219bf596cbd1af9ec9fc0ca8a
whataten <tiktaalik135462@gmail.com>
2024-05-16 16:21:23 +0900

    mariadb를 통한 sql문 실습

--- mariadb-practices/sql-practices/Favorite/practices.sql
+++ mariadb-practices/sql-practices/Favorite/practices.sql
@@ -392,11 +392,290 @@ SELECT ifnull(a.name, '없음') as '이름', b.name as '부서' FROM emp a right
 -- full (outer) join
 -- mariadb 지원 안함
 
+--
+-- subquery
+--
+
+--
+-- 1) select 절, insert into t1 values(...)
+-- 
+
+--
+-- 2) from 절의 서브쿼리
+-- 
+select now() as n, sysdate() as s, 3 + 1 as r from dual;
+select n, s
+  from (select now() as n, sysdate() as s, 3 + 1 as r from dual) a;
+
+--
+-- 3) where 절의 서브쿼리
+-- 
+
+-- 예제) 현재, 'Fai Bale'이 근무하는 부서에서 근무하는 다른 직원의 사번과 이름을 출력
+select b.dept_no
+  from employees a, dept_emp b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+   and concat(a.first_name, ' ', a.last_name) = 'Fai Bale';
+
+-- 'd004'   
+
+select a.emp_no, a.first_name
+  from employees a, dept_emp b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+   and b.dept_no = 'd004';
+
+ 
+select a.emp_no, a.first_name
+  from employees a, dept_emp b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+   and b.dept_no = (select b.dept_no
+                      from employees a, dept_emp b
+                     where a.emp_no = b.emp_no
+                       and b.to_date = '9999-01-01'
+                       and concat(a.first_name, ' ', a.last_name) = 'Fai Bale');
+ 
+ -- 3-1) 단일행 연산자: =, >, <, >=, <=, <>, !=
+ -- 실습문제1
+ -- 현재, 전체 사원의 평균 연봉보다 적은 급여를 받는 사원의 이름과 급여를 출력
+ select avg(salary)
+   from salaries
+  where to_date = '9999-01-01'; 
+ 
+  select a.first_name, b.salary
+    from employees a, salaries b
+   where a.emp_no = b.emp_no
+	 and b.to_date = '9999-01-01'
+     and b.salary < (select avg(salary)
+                       from salaries
+                      where to_date = '9999-01-01')
+order by b.salary desc;                     
+
+-- 실습문제2
+-- 현재, 직책별 평균급여 중에 가장 적은 직책의 직책이름과 그 평균급여를 출력
+-- 1) 직책별 평균 급여
+  select a.title, avg(salary)
+    from titles a, salaries b
+   where a.emp_no = b.emp_no
+     and a.to_date = '9999-01-01'
+     and b.to_date = '9999-01-01'
+group by a.title;
+   
+-- 2) 직책별 가장 적은 평균 급여: from절 subquery
+select min(avg_salary)
+  from (  select a.title, avg(salary) as avg_salary
+            from titles a, salaries b
+           where a.emp_no = b.emp_no
+             and a.to_date = '9999-01-01'
+             and b.to_date = '9999-01-01'
+        group by a.title) a;
+
+-- 3) sol1: where절 subquery(=) 
+  select a.title, avg(salary)
+    from titles a, salaries b
+   where a.emp_no = b.emp_no
+     and a.to_date = '9999-01-01'
+     and b.to_date = '9999-01-01'
+group by a.title
+  having avg(salary) = (select min(avg_salary)
+                          from (  select a.title, avg(salary) as avg_salary
+                                    from titles a, salaries b
+                                   where a.emp_no = b.emp_no
+                                     and a.to_date = '9999-01-01'
+                                     and b.to_date = '9999-01-01'
+								group by a.title) a); 
+ 
+ -- 4) sol2: top-k(limit)
+  select a.title, avg(salary)
+    from titles a, salaries b
+   where a.emp_no = b.emp_no
+     and a.to_date = '9999-01-01'
+     and b.to_date = '9999-01-01'
+group by a.title
+order by avg(salary) asc
+   limit 0, 1;
+ 
+ -- 3-2) 복수행 연산자: in, not in, 비교연산자any, 비교연산자all
+ 
+-- any 사용법
+-- 1. =any: in
+-- 2. >any, >=any: 최소값
+-- 3. <any, <=any: 최대값
+-- 4. <>any, !=any: not in
+
+-- all 사용법
+-- 1. =all: (x)
+-- 2. >all, >=all: 최대값
+-- 3. <all, <=all: 최소값
+-- 4. <>all, !=all
+
+-- 실습문제3
+-- 현재 급여가 50000 이상인 직원의 이름과 급여를 출력
+
+-- sol1) join
+  select a.first_name, b.salary
+    from employees a, salaries b
+   where a.emp_no = b.emp_no
+     and b.to_date = '9999-01-01'
+     and b.salary > 50000
+order by b.salary asc;   
+   
+-- sol2) subquery: where(in)
+select emp_no, salary
+  from salaries
+ where to_date = '9999-01-01'
+   and salary > 50000;
+
+  select a.first_name, b.salary
+    from employees a, salaries b
+   where a.emp_no = b.emp_no
+     and b.to_date = '9999-01-01'
+     and (a.emp_no, b.salary) in (select emp_no, salary
+                                    from salaries
+								   where to_date = '9999-01-01'
+                                     and salary > 50000)
+order by b.salary asc;                                   
+   
+-- sol3) subquery: where(=any)
+select emp_no, salary
+  from salaries
+ where to_date = '9999-01-01'
+   and salary > 50000;
+
+  select a.first_name, b.salary
+    from employees a, salaries b
+   where a.emp_no = b.emp_no
+     and b.to_date = '9999-01-01'
+     and (a.emp_no, b.salary) =any (select emp_no, salary
+                                    from salaries
+								   where to_date = '9999-01-01'
+                                     and salary > 50000)
+order by b.salary asc;
+ 
+-- 실습문제4:
+-- 현재, 각 부서별로 최고 급여를 받고 있는 직원의 부서, 이름과 월급을 출력
+
+  select a.dept_no, max(b.salary)
+    from dept_emp a, salaries b
+   where a.emp_no = b.emp_no
+     and a.to_date = '9999-01-01'
+     and b.to_date = '9999-01-01'
+group by a.dept_no;
 
 
+   
+-- sol1: where절 subquery(in)
+ select a.dept_name, c.first_name, d.salary
+  from departments a,
+       dept_emp b,
+       employees c,
+       salaries d
+ where a.dept_no = b.dept_no
+   and b.emp_no = c.emp_no
+   and c.emp_no = d.emp_no
+   and b.to_date = '9999-01-01'
+   and d.to_date = '9999-01-01'
+   and (b.dept_no, d.salary) in (  select a.dept_no, max(b.salary)
+                                     from dept_emp a, salaries b
+									where a.emp_no = b.emp_no
+                                      and a.to_date = '9999-01-01'
+                                      and b.to_date = '9999-01-01'
+                                 group by a.dept_no);
+   
+   
+-- sol2: from절 subquery & join
+select a.dept_name, c.first_name, d.salary
+  from departments a,
+       dept_emp b,
+       employees c,
+       salaries d,
+       (  select a.dept_no, max(b.salary) as max_salary
+			from dept_emp a, salaries b
+		   where a.emp_no = b.emp_no
+             and a.to_date = '9999-01-01'
+             and b.to_date = '9999-01-01'
+        group by a.dept_no) e
+ where a.dept_no = b.dept_no
+   and b.emp_no = c.emp_no
+   and c.emp_no = d.emp_no
+   and b.dept_no = e.dept_no
+   and b.to_date = '9999-01-01'
+   and d.to_date = '9999-01-01'
+   and d.salary = e.max_salary;
+   
+-- DDL/DML 연습
 
+drop table member;
+create table member(
+	no int not null auto_increment,
+    email varchar(200) not null default '',
+    password varchar(64) not null,
+    name varchar(50) not null,
+    department varchar(100),
+    primary key(no)
+);
+desc member;
 
+alter table member add column juminbunho char(13) not null;
+desc member;
 
+alter table member drop juminbunho;
+desc member;
 
+alter table member add column juminbunho char(13) not null after email;
+desc member;
 
+alter table member change column department dept varchar(100) not null;
+desc member;
 
+alter table member add column self_intro text;
+desc member;
+
+alter table member drop juminbunho;
+desc member;
+
+-- insert
+insert
+  into member
+values (null, 'email@gmail.com', password('1234'), 'ten', 'development', null); 
+select * from member;
+
+insert
+  into member(no, email, name, dept, password)
+values (null, 'email2@gmail.com', 'ten2', 'development', password('1234')); 
+select * from member;
+
+-- update
+update member
+   set email='email3@gmail.com', password=password('4321')
+ where no = 2; 
+select * from member;
+
+-- delete
+delete
+  from member
+ where no = 2;
+select * from member;
+
+-- transaction
+select no, email from member;
+
+select @@autocommit; -- 1
+insert
+  into member(no, email, name, dept, password)
+values (null, 'email2@gmail.com', 'ten2', 'development', password('1234')); 
+select no, email from member;
+
+-- tx begin
+set autocommit = 0;
+select @@autocommit; -- 0
+insert
+  into member(no, email, name, dept, password)
+values (null, 'email3@gmail.com', 'ten3', 'development', password('1234')); 
+select no, email from member;
+
+-- tx end
+commit;
+select no, email from member;
---
+++ mariadb-practices/src/main/java/com/poscodx/ConnectionTest.java
@@ -0,0 +1,33 @@
+package com.poscodx;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionTest {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+
+        try {
+            // 1. JDBC Driver 로딩
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            // 2. 연결하기
+            String url = "jdbc:mariadb://192.168.64.2:3306/webdb?charset=utf-8";
+            connection = DriverManager.getConnection(url, "webdb", "webdb");
+
+            System.out.println("success!");
+        } catch (Exception e) {
+            System.out.println("드라이버 로딩 실패: " + e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
