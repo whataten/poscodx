26f527704a4ae0f7ff3e37b1548dc4cc79c9c209
whataten <tiktaalik135462@gmail.com>
2024-05-22 08:59:34 +0900

    servlet 실습 #1

---
+++ servlet-practices/helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,25 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		pw.println("<h1>안녕 " + id + "</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
