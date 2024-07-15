553c17faa9614ea6d5a4d7cdf614ec3df2061036
whataten <tiktaalik135462@gmail.com>
2024-07-15 10:12:33 +0900

    react 실습 #5 변환 완료

--- react-practices/emaillist08/frontend/src/SearchBar.js
+++ react-practices/emaillist08/frontend/src/SearchBar.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {_SearchBar} from './assets/scss/SearchBar.scss';
+import {_Searchbar} from './assets/scss/SearchBar.scss';
 
 function SearchBar() {
     return (
-        <div className={_SearchBar}>
+        <div className={_Searchbar}>
             <input type='text' placeholder='찾기' />
         </div>
     );
