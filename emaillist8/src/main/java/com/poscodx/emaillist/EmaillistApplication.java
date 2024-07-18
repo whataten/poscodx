a931702f310c0656090e2ce7ff10941557e2c78e
whataten <tiktaalik135462@gmail.com>
2024-07-18 14:59:50 +0900

    emaillist backend #2

--- emaillist8/src/main/java/com/poscodx/emaillist/EmaillistApplication.java
+++ emaillist8/src/main/java/com/poscodx/emaillist/EmaillistApplication.java
@@ -3,6 +3,8 @@ package com.poscodx.emaillist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.poscodx.emaillist.vo.EmaillistVo;
+
 @SpringBootApplication
 public class EmaillistApplication {
 	public static void main(String[] args) {
